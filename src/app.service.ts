0e2a274a0254e106ec30317c63053a03179bc8df
Zeeshan Ashraf <zeeshan.ashraf@dastgyr.com>
2023-02-26 13:27:37 +0500

    Text Completion API Intregated

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,10 @@ import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 
 // Endpoint URL
-const endpointUrl = 'https://api.openai.com/v1/models';
+const endpointUrl = {
+  models: 'https://api.openai.com/v1/models',
+  completions: 'https://api.openai.com/v1/completions',
+};
 
 @Injectable()
 export class AppService {
@@ -11,12 +14,27 @@ export class AppService {
 
   async getModel() {
     try {
-      const bodyParameters = {
-        key: 'value',
-      };
-
       const response = await axios.get(
-        endpointUrl,
+        endpointUrl.models,
+        this.getHeaderParameters(this.configService),
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  async createCompletion() {
+    try {
+      const bodyParameters = JSON.stringify({
+        model: "text-davinci-002",
+        prompt: "Write about artificial intelligence.",
+        temperature: 0.9
+      });
+      const response = await axios.post(
+        endpointUrl.completions,
+        bodyParameters,
         this.getHeaderParameters(this.configService),
       );
       return response.data;
@@ -29,6 +47,7 @@ export class AppService {
     const config = {
       headers: {
         Authorization: `Bearer ${configService.get<string>('SECRET_KEY')}`,
+        "Content-Type": "application/json"
       },
     };
     return config;
